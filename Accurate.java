5b86bf2da56022cd4ea8e2fa3ed412221299b16b
TsungYu-Chen <bleach1395@gmail.com>
2016-05-09 01:05:45 +0800

    Update Accurate.java
    把BigInteger改成BigDecimal &加註解

--- Accurate.java
+++ Accurate.java
@@ -1,7 +1,7 @@
-import java.math.BigInteger;
+import java.math.BigDecimal;
 
- 
 public class Accurate{
+	//method for add number
 	public static double add(double a, double b){
 		BigDecimal num1;
 		BigDecimal num2;
@@ -11,6 +11,7 @@ public class Accurate{
 		
 		return num1.add(num2).doubleValue();
 	}
+	//method for subtract number
 	public static double sub(double a, double b){
 		BigDecimal num1;
 		BigDecimal num2;
@@ -20,6 +21,7 @@ public class Accurate{
 		
 		return num1.subtract(num2).doubleValue();
 	}
+	//method for mutiply number
 	public static double mul(double a, double b){
 		BigDecimal num1;
 		BigDecimal num2;
