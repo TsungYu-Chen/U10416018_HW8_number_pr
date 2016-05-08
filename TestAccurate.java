5733fbc4c46a173ee0169c996fd49a5c005209e8
TsungYu-Chen <bleach1395@gmail.com>
2016-05-09 01:10:44 +0800

    Update TestAccurate.java
    加註解

--- TestAccurate.java
+++ TestAccurate.java
@@ -1,17 +1,20 @@
+//U10416018 陳宗佑
+
 import java.util.Scanner;
 
 public class TestAccurate{
 	
 	public static void main(String[] args){
-		
+		//Prompt user to enter two number
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the first number:");
 		Double a = input.nextDouble();
 		System.out.println("Enter the second number:");
 		Double b = input.nextDouble();
 		
+		//Call class Accrate
 		Accurate accurate = new Accurate();
-		
+		//Ask what way user want to do
 		System.out.println("Choose what way you want to calculate: +, -, * ? ");
 		char determine = (input.next()).charAt(0);
 		
